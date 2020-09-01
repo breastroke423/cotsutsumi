83c38067caf571751369789fe59c7b54b9b99cee
vagrant <vagrant@localhost.localdomain>
2020-09-01 14:54:43 +0000

    update controller user,want,waste,app / add views / fix devise view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+
+
+  def mypage
+  end
 end
--- app/controllers/wastes_controller.rb
+++ app/controllers/wastes_controller.rb
@@ -1,2 +1,12 @@
 class WastesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
 end
