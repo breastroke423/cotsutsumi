ae3f3434c4579460a8081ce3fe7ff2b69e3c8096
vagrant <vagrant@localhost.localdomain>
2020-09-01 09:53:01 +0000

    add view top,about,header / update routes / update controller homes /

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :users, only:[:index, :show, :update, :edit]
+resources :wants, only:[:index, :edit, :update]
+resources :wastes, only:[:index, :show, :edit]
+resources :wants do
+  resource :cheers, only:[:create, :destoroy]
+end
+
+
+
+root 'homes#top'
+get 'homes/about'
+
 end
