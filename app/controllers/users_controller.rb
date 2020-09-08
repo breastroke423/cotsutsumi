e40c38280c44984b3e7f97ffd4ceb80423883a0c
vagrant <vagrant@localhost.localdomain>
2020-09-08 08:42:06 +0000

    fix view, add turbolinks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
 
   def index
+    if user_signed_in?
     @users = User.where.not(id: current_user.id)
+  else
+    @users = User.all
+    end
   end
 
   def show
