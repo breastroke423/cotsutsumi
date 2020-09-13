f8616972dbc7f05a83b18ecb7cf06d3b5101ca59
vagrant <vagrant@localhost.localdomain>
2020-09-13 04:41:48 +0000

    follow asynchronous

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,23 @@
 class RelationshipsController < ApplicationController
 
+
   def create
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
-    follow.save
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      follow.save
+    format.js
+    end
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_back(fallback_location: root_path)
+    respond_to do |format|
+      follow.destroy
+    format.js
   end
+end
 
 end
