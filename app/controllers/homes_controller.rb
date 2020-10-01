c75819240669034ce8c8333361213174f15eead1
vagrant <vagrant@localhost.localdomain>
2020-10-01 07:38:19 +0000

    add helper clculation/ update controller users,wastes,home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+  include ApplicationHelper
 
   def top
     if user_signed_in?
@@ -8,29 +9,16 @@ class HomesController < ApplicationController
       @users = User.where(is_deleted: false).sample(3)
     end
 
-# 特定のユーザーの無駄遣いの合計を計算
+    # 特定のユーザーの無駄遣いの合計を計算
     @wastes = Waste.all
-    @user_total_price = 0
-    @wastes.each do |waste|
-      waste_total = waste.price * waste.count
-      @user_total_price+=waste_total
-    end
+    calculation_waste_purchase
+    calculation_waste_total
 
-# 全ユーザーの無駄遣いの合計を求める
-    @user_all = User.all
-    @users_wastes_all = 0
-    @user_all.each do |user|
-      user.wastes.each do |waste|
-        waste_all_total = waste.price * waste.count
-        @users_wastes_all+=waste_all_total
-      end
-    end
+    # 全ユーザーの無駄遣いの合計を求める
+    all_user_wast_total
 
-# 全ユーザーの目標達成額を計算する
-    @users_purchase_all = 0
-    @user_all.each do |user|
-      @users_purchase_all+=user.purchase_price
-    end
+    # 全ユーザーの目標達成額を計算する
+    all_user_purchase_total
   end
 
   def about
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:update, :edit, :achievement, :mypage, :follows, :followers, :hide]
+  include ApplicationHelper
+
 
   def index
   # ランダム表示
@@ -11,86 +13,59 @@ class UsersController < ApplicationController
   @want = Want.new
 end
 
-def update
-  @user = current_user
-  if @user.update(user_params)
-    redirect_to mypage_path
-  else
+  def update
     @user = current_user
-    @user.profile_image = current_user.profile_image
-    render 'edit'
+    if @user.update(user_params)
+      redirect_to mypage_path
+    else
+      @user = current_user
+      @user.profile_image = current_user.profile_image
+      render 'edit'
+    end
   end
-end
 
-def edit
-  @user = current_user
-end
-
-def achievement
-end
-
-def mypage
-  @user = current_user
-  @wastes = Waste.where(user_id: @user.id)
-
-# 自分自身の無駄遣い削減合計
-@user_total_price = 0
-@wastes.each do |waste|
-  waste_total = waste.price * waste.count
-  @user_total_price+=waste_total
-end
-
-# 現在の積みたて＝今使ってもいい額＝無駄遣い削減合計ー目標達成の購入額
-@user_difference_price = @user_total_price - @user.purchase_price
-@waste_count_all = 0
-@wastes.each do |waste|
-  @waste_count_all += waste.count
-end
-
-# 全ユーザーの無駄遣い削減額
-@user_all = User.all
-@users_wastes_all = 0
-@user_all.each do |user|
-  user.wastes.each do |waste|
-    waste_all_total = waste.price * waste.count
-    @users_wastes_all+=waste_all_total
+  def edit
+    @user = current_user
   end
-end
 
-# 全ユーザーの目標達成購入額
-@users_purchase_all = 0
-@user_all.each do |user|
-  @users_purchase_all+=user.purchase_price
-end
-end
-
-def follows
-  @users = current_user.followings.where(is_deleted: false).page(params[:page]).per(5)
-end
-
-def followers
-  @users = current_user.followers.where(is_deleted: false).page(params[:page]).per(5)
-end
-
-def hide # 退会オプション、destroyにしないのは合計額が崩れるから
-  @user = User.find(params[:id])
-  @user.update(is_deleted: true)
-  # default = "f"
-  reset_session
-  flash[:thanks] = "買いたいものができたときのご利用お待ちしております"
-  redirect_to root_path
-end
-
-def search # 検索用
-  if params[:nickname].present?
-    @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%").page(params[:page]).per(5)
-  else
-    @users = User.none.page(params[:page]).per(5)
+  def achievement
   end
-end
 
-  private
-  def user_params
-    params.require(:user).permit(:nickname, :profile_image, :introduction, :email)
+  def mypage
+    @user = current_user
+    @wastes = Waste.where(user_id: @user.id)
+    calculation_waste_purchase
+    # 現在の積みたて＝今使ってもいい額＝無駄遣い削減合計ー目標達成の購入額
+    calculation_possible_total
   end
+
+  def follows
+    @users = current_user.followings.where(is_deleted: false).page(params[:page]).per(5)
+  end
+
+  def followers
+    @users = current_user.followers.where(is_deleted: false).page(params[:page]).per(5)
+  end
+
+  def hide # 退会オプション、destroyにしないのは合計額が崩れるから
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    # default = "f"
+    reset_session
+    flash[:thanks] = "買いたいものができたときのご利用お待ちしております"
+    redirect_to root_path
+  end
+
+  def search # 検索用
+    if params[:nickname].present?
+      @users = User.where('nickname LIKE ?', "%#{params[:nickname]}%").page(params[:page]).per(5)
+    else
+      @users = User.none.page(params[:page]).per(5)
+    end
+  end
+
+    private
+    def user_params
+      params.require(:user).permit(:nickname, :profile_image, :introduction, :email)
+    end
 end
