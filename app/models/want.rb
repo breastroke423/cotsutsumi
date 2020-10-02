c993ef47a9a62fbf01f90e487a37c85babea860d
Mitsuhiro.Akeda <mitsuhiro.akeda@MitsuhiroAkedanoMacBook-Air.local>
2020-10-02 16:34:55 +0900

    fix indent

--- app/models/want.rb
+++ app/models/want.rb
@@ -4,10 +4,9 @@ class Want < ApplicationRecord
   validates :name, presence: true, length: {maximum: 15}
   validates :price, numericality: { only_integer: true }
   validates :reason, length: {maximum: 15}
-
+  attachment :want_image, content_type: ["image/jpeg", "image/png"]
 
   def cheered_by?(user)
     cheers.where(user_id: user.id).exists?
   end
-  attachment :want_image, content_type: ["image/jpeg", "image/png"]
 end
